647ce7e520fc8a747df89f1ec7bb88bcdc5f733f
Ayush Singh <asayushsingh80@gmail.com>
2023-03-22 20:46:04 +0530

    Some changes

--- schema/pet.js
+++ schema/pet.js
@@ -4,16 +4,16 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const PetSchema = new Schema({
-    id: ObjectId,
-    nameOfPet: {type: String, required: true},
-    nameOfOwner:  {type: String, required: true},
-    age:  {type: Number, required: true},
-    type:  {type: String, required: true},
-    gender:  {type: String, required: true},
-})
+  id: ObjectId,
+  nameOfPet: { type: String, required: true },
+  nameOfOwner: { type: String, required: true },
+  age: { type: Number, required: true, min: 10, max: 100 },
+  type: { type: String, required: true },
+  gender: { type: String, required: true, lowercase: true },
+});
 
 const PetModel = mongoose.model("Pet", PetSchema);
 
 module.exports = {
-    Pet: PetModel
-}
+  Pet: PetModel,
+};
